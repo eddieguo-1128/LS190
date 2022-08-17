1465404feba0557c57aa681c48424561f0644998
eddieguo-1128 <70269257+eddieguo-1128@users.noreply.github.com>
2022-08-17 15:09:48 -0700

    Add files via upload

---
+++ extract_cases.py
@@ -0,0 +1,20 @@
+import os
+import sys
+sys.path.append('..')
+
+import lzma
+import json
+
+from config import settings
+import utils
+
+def get_cases(state):
+    compressed_file = utils.get_cases_from_bulk(jurisdiction=state, data_format="json")
+    
+    cases = []
+    print("File path:", compressed_file)
+    with lzma.open(compressed_file) as infile:
+        for line in infile:
+            record = json.loads(str(line, 'utf-8'))
+            cases.append(record)
+    return cases
